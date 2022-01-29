7e6c42b838cbc382ec0ee63342d4da968f3e2c80
huybn5776 <aseeme555076@gmail.com>
2022-01-29 16:25:25 +0800

    feat: allow ItemImage to show with limited width or height

--- src/components/ItemImage/ItemImage.vue
+++ src/components/ItemImage/ItemImage.vue
@@ -1,52 +1,94 @@
 <template>
-  <div class="item-image-container" :style="{ width: `${size}px`, height: `${size}px` }">
-    <img class="item-image" loading="lazy" :alt="item.name" :src="imageInfo?.src" :style="imageInfo?.style" />
+  <div
+    class="item-image-container"
+    :style="{ width: `${width || size || imageInfo?.width}px`, height: `${height || size || imageInfo?.height}px` }"
+  >
+    <img
+      class="item-image"
+      loading="lazy"
+      :alt="name || item?.name"
+      :src="imageInfo?.src"
+      :style="imageInfo?.style"
+      :class="{ loading: !loaded }"
+      @load="loaded = true"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { StyleValue, computed } from 'vue';
+import { StyleValue, computed, ref } from 'vue';
 
 import { ItemPrefab } from '@interfaces/Item-prefab';
 import { Rect } from '@interfaces/rect';
 import { SpriteImage } from '@interfaces/sprite';
 
 interface ItemImageInfo {
-  src?: string;
-  style?: StyleValue;
+  src: string;
+  style: StyleValue;
+  width: number;
+  height: number;
 }
 
-const props = defineProps<{ item: ItemPrefab; size?: number }>();
-const size = props.size || 64;
+const props = defineProps<{
+  item?: ItemPrefab;
+  image?: SpriteImage;
+  size?: number;
+  width?: number;
+  height?: number;
+  name?: string;
+}>();
 
-const imageInfo = computed(() => getItemImageInfo(props.item));
+const loaded = ref(false);
+const imageInfo = computed(() => getItemImageInfo(props.image || props.item?.infectedIcon || props.item?.sprite));
 
-function getItemImageInfo(item: ItemPrefab): ItemImageInfo | undefined {
-  const sprite = item.infectedIcon || item.sprite;
+function getItemImageInfo(sprite: SpriteImage | undefined): ItemImageInfo | undefined {
   if (!sprite) {
-    console.error(`Cannot find image of item: ${item.identifier} ${item.englishName || item.name}`);
+    console.error(`Cannot find image of item: ${props.name || props.item?.name}`);
     return undefined;
   }
 
   const rect = getRectFromSprite(sprite);
   if (!rect) {
-    console.error(`Cannot get react from item sprite: ${item.identifier} ${item.englishName || item.name}`);
+    console.error(`Cannot get react from item sprite: ${props.name || props.item?.name}`);
     return undefined;
   }
-  const { x, y, width, height } = rect;
-  const targetSize = props.size || 64;
-  const zoom = Math.min(targetSize / width, targetSize / height);
+  const { x, y, width: originalWidth, height: originalHeight } = rect;
+  const { width, height, zoom } = calcSizeAndZoom(originalWidth, originalHeight);
 
   return {
     src: `/data-source/images/${sprite?.texture}`,
     style: {
       objectPosition: `${-x}px ${-y}px`,
-      width: `${width}px`,
-      height: `${height}px`,
-      transform: `scale(${zoom})`,
+      width: `${originalWidth}px`,
+      height: `${originalHeight}px`,
+      transform: zoom ? `scale(${zoom})` : undefined,
       transformOrigin: 'center',
     },
+    width,
+    height,
   };
+}
+
+function calcSizeAndZoom(
+  originalWidth: number,
+  originalHeight: number,
+): { width: number; height: number; zoom: number } {
+  let width = props.width || props.size || 0;
+  let height = props.height || props.size || 0;
+  let zoom = 0;
+  if (width && height) {
+    zoom = Math.min(width / originalWidth, height / originalHeight);
+  } else if (!width && !height) {
+    width = originalWidth;
+    height = originalHeight;
+  } else if (width) {
+    zoom = width / originalWidth;
+    height = originalHeight * zoom;
+  } else {
+    zoom = height / originalHeight;
+    width = originalWidth * zoom;
+  }
+  return { width, height, zoom };
 }
 
 function getRectFromSprite(sprite: SpriteImage): Rect | undefined {
