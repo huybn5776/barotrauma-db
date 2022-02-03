dcc765a5f9c72eceb43b15df71c06e06144a8ad9
huybn5776 <aseeme555076@gmail.com>
2022-02-03 14:05:42 +0800

    feat: override search hotkey to focus on quick filter

--- src/components/ItemQuickFilter/ItemQuickFilter.vue
+++ src/components/ItemQuickFilter/ItemQuickFilter.vue
@@ -6,10 +6,13 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, watch, watchEffect } from 'vue';
+import { ref, watch, watchEffect, onMounted } from 'vue';
+
+import { fromEvent, filter, take, takeUntil, switchMapTo } from 'rxjs';
 
 import SearchInput from '@components/SearchInput/SearchInput.vue';
 import { useDebounce } from '@compositions/use-debounce';
+import { useUntilDestroyed } from '@compositions/use-until-destroyed';
 
 const props = defineProps<{ modelValue?: string }>();
 const emits = defineEmits<{ (e: 'update:modelValue', value: string): void }>();
@@ -18,6 +21,19 @@ const inputRef = ref<{ input: HTMLInputElement }>();
 const expanded = ref(false);
 const value = ref('');
 const debouncedSearchTerm = useDebounce(value, { immediatelyClear: true });
+
+const untilDestroyed = useUntilDestroyed();
+const searchHotKey$ = fromEvent<KeyboardEvent>(document.body, 'keydown').pipe(
+  filter((event) => (event.metaKey || event.ctrlKey) && event.code === 'KeyF'),
+  untilDestroyed(),
+);
+
+onMounted(() => {
+  searchHotKey$.pipe(take(1)).subscribe((event) => {
+    expanded.value = true;
+    event.preventDefault();
+  });
+});
 
 watchEffect(() => (value.value = props.modelValue || ''));
 watch(debouncedSearchTerm, () => emits('update:modelValue', debouncedSearchTerm.value));
@@ -28,6 +44,14 @@ watch(inputRef, () => {
     return;
   }
   inputElement.focus();
+  fromEvent(inputElement, 'blur')
+    .pipe(switchMapTo(searchHotKey$.pipe(take(1), takeUntil(fromEvent(inputElement, 'focus')))), untilDestroyed())
+    .subscribe((event) => {
+      if (inputRef.value?.input) {
+        inputRef.value?.input.focus();
+        event.preventDefault();
+      }
+    });
 });
 </script>
 
