35a95b8cc0bf418ad5b37b599f2b06cf8e663346
huybn5776 <aseeme555076@gmail.com>
2022-02-15 23:37:21 +0800

    fix: limit location icon anchor to as height as icon

--- src/components/LocationIcon/LocationIcon.vue
+++ src/components/LocationIcon/LocationIcon.vue
@@ -1,43 +1,48 @@
 <template>
-  <a href="https://barotraumagame.com/wiki/Campaign#Habitation_Outpost" target="_blank">
-    <img
-      v-if="type === LocationType.Outpost"
-      class="location-image"
-      src="/src/assets/icon/location-habitation-outpost.png"
-      alt="Habitation"
-    />
+  <a
+    v-if="type === LocationType.Outpost"
+    class="location-link"
+    href="https://barotraumagame.com/wiki/Campaign#Habitation_Outpost"
+    target="_blank"
+    title="Habitation"
+  >
+    <img class="location-image" src="/src/assets/icon/location-habitation-outpost.png" alt="Habitation" />
   </a>
-  <a href="https://barotraumagame.com/wiki/Campaign#Colony" target="_blank">
-    <img
-      v-if="type === LocationType.City"
-      class="location-image"
-      src="/src/assets/icon/location-colony.png"
-      alt="Colony"
-    />
+  <a
+    v-else-if="type === LocationType.City"
+    class="location-link"
+    href="https://barotraumagame.com/wiki/Campaign#Colony"
+    target="_blank"
+    title="Colony"
+  >
+    <img class="location-image" src="/src/assets/icon/location-colony.png" alt="Colony" />
   </a>
-  <a href="https://barotraumagame.com/wiki/Campaign#Research_Outpost" target="_blank">
-    <img
-      v-if="type === LocationType.Research"
-      class="location-image"
-      src="/src/assets/icon/location-research-outpost.png"
-      alt="Research"
-    />
+  <a
+    v-else-if="type === LocationType.Research"
+    class="location-link"
+    href="https://barotraumagame.com/wiki/Campaign#Research_Outpost"
+    target="_blank"
+    title="Research"
+  >
+    <img class="location-image" src="/src/assets/icon/location-research-outpost.png" alt="Research" />
   </a>
-  <a href="https://barotraumagame.com/wiki/Campaign#Military_Outpost" target="_blank">
-    <img
-      v-if="type === LocationType.Military"
-      class="location-image"
-      src="/src/assets/icon/location-military-outpost.png"
-      alt="Military"
-    />
+  <a
+    v-else-if="type === LocationType.Military"
+    class="location-link"
+    href="https://barotraumagame.com/wiki/Campaign#Military_Outpost"
+    target="_blank"
+    title="Military"
+  >
+    <img class="location-image" src="/src/assets/icon/location-military-outpost.png" alt="Military" />
   </a>
-  <a href="https://barotraumagame.com/wiki/Campaign#Mining_Outpost" target="_blank">
-    <img
-      v-if="type === LocationType.Mine"
-      class="location-image"
-      src="/src/assets/icon/location-mining-outpost.png"
-      alt="Mining"
-    />
+  <a
+    v-else-if="type === LocationType.Mine"
+    class="location-link"
+    href="https://barotraumagame.com/wiki/Campaign#Mining_Outpost"
+    target="_blank"
+    title="Mining"
+  >
+    <img class="location-image" src="/src/assets/icon/location-mining-outpost.png" alt="Mining" />
   </a>
 </template>
 
